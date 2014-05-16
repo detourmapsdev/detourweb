518d8cadfbc741f55d5f61ced68632f35ce7fa1e
root <root@qmoqui.com>
2014-05-16 21:53:22 +0000

    Backup bd postgres

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ DATABASES = {
         # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'dev_geo_detour', # Or path to database file if using sqlite3.
         'USER': 'postgres', # Not used with sqlite3.
-        'PASSWORD': '04718802', # Not used with sqlite3.
+        'PASSWORD': 'd3t0urm4ps-s3rv3r', # Not used with sqlite3.
         'HOST': 'localhost', # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432', # Set to empty string for default. Not used with sqlite3.
     }
