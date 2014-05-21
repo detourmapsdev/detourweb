7ff55ddca2c919d247fe5d56f10018530130199f
Jayme <kalkehcoisa@gmail.com>
2014-05-21 20:23:40 -0300

    removed admin docs from urls.py

--- urls.py
+++ urls.py
@@ -23,7 +23,7 @@ urlpatterns = patterns('',
     (r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root': media,'show_indexes': True}),
     #end static files
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
